42ecd80bd0853258f9d25eab1558d55b48cddfb6
inemtsev <ilya.nemtsev@outlook.com>
2015-10-25 11:30:42 -0400

    init commit

---
+++ DbLibs/AdoDb.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbLibs.Interfaces;
+using DbLibs.Properties;
+using DbLibs.Models;
+
+namespace DbLibs
+{
+    public class AdoDb : IDataAccess
+    {
+
+        public IEnumerable<Product> Read(int numberOfProducts)
+        {
+            string query = string.Format("SELECT TOP {0} [ProductId],[ProductName],[ProductDescription],[Quantity],[IsOnSale] FROM BenchDb", numberOfProducts);
+            using (var connection = new SqlConnection(Settings.Default.ProductContext))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            yield return new Product()
+                            {
+                                ProductId = int.Parse(reader["ProductId"].ToString()),
+                                ProductName = reader["ProductName"].ToString(),
+                                ProductDescription = reader["ProductDescription"].ToString(),
+                                Quantity = int.Parse(reader["Quantity"].ToString()),
+                                IsOnSale = bool.Parse(reader["IsOnSale"].ToString())
+                            };
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+        }
+
+        public void Insert(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DbLibs/EfDb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using DbLibs.Interfaces;
+using DbLibs.Models;
+using DbLibs.Properties;
+
+namespace DbLibs
+{
+    public class EfDb : IDataAccess
+    {
+        readonly ProductContext _productsDb = new ProductContext();
+
+
+        public IEnumerable<Product> Read(int numberOfProducts)
+        {
+            var products = _productsDb.Products.Take(numberOfProducts).ToList();
+            return products;
+        }
+
+        public void Insert(Product product)
+        {
+            _productsDb.Products.Add(product);
+        }
+    }
+
+    public class ProductContext : DbContext
+    {
+        public ProductContext()
+            : base(Settings.Default.ProductContext)
+        {   
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
